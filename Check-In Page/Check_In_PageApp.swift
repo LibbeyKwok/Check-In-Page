7ef644e5a64b97db67265115364ec623c09744fc
Libbey Kwok <libbeykwok8@gmail.com>
2023-07-19 13:46:47 -0700

    Initial Commit

---
+++ Check-In Page/Check_In_PageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Check_In_PageApp.swift
+//  Check-In Page
+//
+//  Created by Libbey Kwok on 7/19/23.
+//
+
+import SwiftUI
+
+@main
+struct Check_In_PageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

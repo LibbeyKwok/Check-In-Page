f2e0ccf1d50e39695338e79f44daeb69af2c92d7
LibbeyKwok <libbeykwok8@gmail.com>
2023-07-20 11:24:03 -0700

    yay

--- Check-In Page/ContentView.swift
+++ Check-In Page/ContentView.swift
@@ -11,14 +11,15 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var happyText = " "
-    @State private var mehText = " "
-    @State private var sadText = " "
+    @State private var text = " "
+//    @State private var mehText = " "
+//    @State private var sadText = " "
     @State private var num = 0
     @State private var num1 = 0
     @State private var num2 = 0
     var body: some View {
         VStack {
+            
             Text("Check-in")
                 .font(.title)
             Text("How are you feeling?")
@@ -26,11 +27,11 @@ struct ContentView: View {
                 Button(action:{
                     if (num==0)
                     {
-                        happyText = "Yay! Let's keep it going. Write down 5 things you're grateful for in your journal."
+                       text = "Yay! Let's keep it going. Write down 5 things you're grateful for in your journal."
                         num=1
                     }
                     else {
-                        happyText = " "
+                        text = " "
                         num = 0
                     }
                     
@@ -38,33 +39,35 @@ struct ContentView: View {
                     Image("smiling face")
                         .resizable(resizingMode: .stretch)
                         .aspectRatio(contentMode: .fit)
+                        .frame(width: 110.0)
                 }
                
                 Button(action:{
                     if (num1 == 0)
                     {
-                        mehText = "Things could be better and they also could be worse. Push through! "
+                        text = "Things could be better and they also could be worse. Push through! "
                         num1 = 1
                     }
                     else
                     {
-                        mehText = ""
+                        text = ""
                         num1 = 0
                     }
                 }) {
                     Image("meh face")
                         .resizable(resizingMode: .stretch)
                         .aspectRatio(contentMode: .fit)
+                        .frame(width: 100.0)
                 }
                 Button(action:{
                     if (num2 == 0)
                     {
-                        sadText = "Everybody has their days. It is important to know that your feelings are valid and it will always get better. Write down your feelings in your journal. "
+                        text = "Everybody has their days. It is important to know that your feelings are valid and it will always get better. Write down your feelings in your journal. "
                         num2 = 1
                     }
                     else
                     {
-                        sadText = ""
+                        text = ""
                         num2 = 0
                     }
                     
@@ -72,22 +75,38 @@ struct ContentView: View {
                     Image("sad face")
                         .resizable(resizingMode: .stretch)
                         .aspectRatio(contentMode: .fit)
+                        .frame(width: 120.0)
                 }
             }
-            Text(happyText)
-            Text(mehText)
-            Text(sadText)
+            Text(text)
+                .padding()
             ZStack{
                 RoundedRectangle(cornerRadius: 20)
                     .fill(Color(hex: "CDD7B6"))
-                VStack {
+                VStack(alignment: .leading) {
                     Text("Resources")
                         .font(.title)
+                    Text("------------------")
+                    Text("Substance Abuse and Mental Health Services Administration Hotline: 1-800-662-HELP (4357)")
+                    Link("\n StarVista Crisis Center",
+                          destination: URL(string: "https://star-vista.org/programs/crisis-center/")!)
+                    Link("\n National Alliance on Mental Illness Helplines ",
+                          destination: URL(string: "https://www.nami.org/help")!)
+                    Link("\n The Foundation for Art & Healing",
+                          destination: URL(string: "https://www.artandhealing.org/community-programs-2/")!)
+                    
+                    
+                                
+                    
+                    
+                    
                     
                 }
+                .padding()
             }
+            .padding()
         }
-        .padding()
+        
     }
 }
 
